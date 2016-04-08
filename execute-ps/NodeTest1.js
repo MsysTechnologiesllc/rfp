5d7be3deb73e200a9b64660e0d5cdd60c049e6df
yogiraj84 <yogiraj.ghumade@msystechnologies.com>
2016-04-08 15:18:50 +0530

    Update NodeTest1.js

--- execute-ps/NodeTest1.js
+++ execute-ps/NodeTest1.js
@@ -2,44 +2,13 @@ var assert = require('assert'),
 test = require('selenium-webdriver/testing'),
 webdriver = require('selenium-webdriver');
 var rdp = require('node-rdp');
-
-test.describe('Google Search', function() {
-    test.it('should work', function() {
-       var driver = new webdriver.Builder().
-           withCapabilities(webdriver.Capabilities.chrome()).
-           build();
-       driver.get('http://www.google.com');
-       var searchBox = driver.findElement(webdriver.By.name('q'));
-	   this.timeout(500000);
-       searchBox.sendKeys('Selenium Downloads');
-       searchBox.getAttribute('value').then(function(value) {
-            assert.equal(value, 'Selenium Downloads');
-       });
-       driver.quit();
-     });
-
-});
-
-test.it('Get OS Details', function(){
-var spawn = require("child_process").spawn,child;
-
-// powershell script path to get the OS details.
-child = spawn("powershell.exe",["C:\\Users\\clogeny\\Desktop\\Yogiraj\\Selenium-NodeJS-Mocha\\GetOS.ps1"]); 
-child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-});
-child.stderr.on("data",function(data){
-    console.log("Powershell Errors: " + data);
-});
-child.on("exit",function(){
-    console.log("Powershell Script finished");
-});
-child.stdin.end(); //end input
-	})
+var fs = require('fs');
 
 
-test.it('Create Dir', function(){
-	var spawn = require("child_process").spawn,child;
+//sample code which navigates to google home,captures the screenshot and saves at a newly created drectory from powershell script..
+test.describe('GoogleScrenshot', function() {
+	test.it('Create a folder', function() {
+		var spawn = require("child_process").spawn,child;
 child = spawn("powershell.exe",["C:\\Users\\clogeny\\Desktop\\Yogiraj\\Selenium-NodeJS-Mocha\\CreateDir.ps1"]);
 child.stdout.on("data",function(data){
     console.log("Powershell Data: " + data);
@@ -50,20 +19,41 @@ child.stderr.on("data",function(data){
 child.on("exit",function(){
     console.log("Powershell Script finished");
 });
-child.stdin.end(); //end input		
+child.stdin.end(); //end input
+		
+	});
+test.it('Take scren shot', function() {
+		 var driver = new webdriver.Builder().
+           withCapabilities(webdriver.Capabilities.chrome()).
+           build();
+		driver.get('http://www.google.com');
+		this.timeout(500000);
+		
+function writeScreenshot(data, name) {
+  name = name || 'ss.png';
+  var screenshotPath = 'E:\\Test\\newDir0\\';
+  fs.writeFileSync(screenshotPath + name, data, 'base64');
+};
 
+driver.takeScreenshot().then(function(data) {
+  writeScreenshot(data, 'out1.png');
+});
 });
 
-
-
-var rdp = require('node-rdp');
- 
-rdp({
-  address: '<IP Address Here>',
-  username: '<username>',
+ rdp({
+  address: '<IP Addree Here>',
+  username: '<domain\username>',
   password: '<password>',
   safeMode: true
 }).then(function() {
   console.log('At this, point, the connection has terminated.');
 });
+	
+	
+});
+	
+
+	
+
+
 
